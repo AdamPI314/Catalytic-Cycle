a767c84ef2e01dd6ac25a1c57112bd4d6509b971
AdamPI314 <shirong.bai@colorado.edu>
2017-08-14 01:06:42 -0600

    tiny changes.

--- update_settings.py
+++ update_settings.py
@@ -47,7 +47,7 @@ def update_mc_trajectory_setting(file_dir, time=1.0):
     setting['time']['critical_time'] = time
     setting['time']['max_time'] = time
     setting['time']['path_end_time'] = time
-    setting['pathway']['trajectoryNumber'] = 1000000
+    setting['pathway']['trajectoryNumber'] = 10000000
 
     setting['job']['job_type'] = "generate_pathway_running_Monte_carlo_trajectory"
     rwc.write_configuration(setting, os.path.join(
@@ -71,6 +71,6 @@ def update_eval_path_integral(file_dir, top_n=5):
     setting['job']['job_type'] = "evaluate_path_integral_over_time"
     setting['pathway']['pathwayEndWith'] = "ALL"
     setting['pathway']['topN'] = [top_n]
-    setting['pathway']['trajectoryNumber'] = 10000
+    setting['pathway']['trajectoryNumber'] = 1000000
     rwc.write_configuration(setting, os.path.join(
         file_dir, 'input', 'setting.json'))
