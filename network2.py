be9522c6a86c7c2c4b4727537c86d56fb80ed521
AdamPI314 <shirong.bai@colorado.edu>
2017-08-31 10:12:30 +0800

    change network nodes' weight.

--- network2.py
+++ network2.py
@@ -12,27 +12,40 @@ from plotly.graph_objs import Figure, Data, Font, Annotation, Annotations, \
     Margin, XAxis, YAxis, ZAxis, Scene, Layout, Line, Scatter3d, Marker
 
 
-def plotly_plot_network(file_dir):
+def plotly_plot_network(file_dir, f_name):
     """
     plotly test
     """
     data = []
-    with open(os.path.join(file_dir, "output", "miserables.json"), 'r') as f_h:
+    filename = os.path.join(file_dir, "output", f_name)
+    with open(filename, 'r') as f_h:
         data = json.loads(f_h.read())
 
     n_n = len(data['nodes'])
     # Define the list of edges and the Graph object from edges
     n_l = len(data['links'])
+    g_o = ig.Graph(directed=True)
+    for _, val in enumerate(data['nodes']):
+        g_o.add_vertex(name=val['name'], id=val['id'], weight=val['weight'])
+    for _, val in enumerate(data['links']):
+        g_o.add_edge(source=val['source'],
+                     target=val['target'], weight=val['weight'])
+
     edges = [(data['links'][k]['source'], data['links'][k]['target'])
              for k in range(n_l)]
+    weights_e = []
+    for _, val in enumerate(data['links']):
+        weights_e.append(float(val['weight']))
 
-    g_o = ig.Graph(edges, directed=False)
-
-    labels = []
-    group = []
-    for node in data['nodes']:
-        labels.append(node['name'])
-        group.append(node['group'])
+    print(g_o.vertex_attributes())
+    print(g_o.edge_attributes())
+    ids_n = []
+    names_n = []
+    weights_n = []
+    for _, val in enumerate(data['nodes']):
+        ids_n.append(val['id'])
+        names_n.append(val['name'])
+        weights_n.append(float(val['weight']))
 
     # Get the node positions, set by the Kamada-Kawai layout for 3D graphs
     layt = g_o.layout('kk', dim=3)
@@ -44,17 +57,17 @@ def plotly_plot_network(file_dir):
     x_e = []
     y_e = []
     z_e = []
-    for e in edges:
+    for edge in edges:
         # x-coordinates of edge ends
-        x_e += [layt[e[0]][0], layt[e[1]][0], None]
-        y_e += [layt[e[0]][1], layt[e[1]][1], None]
-        z_e += [layt[e[0]][2], layt[e[1]][2], None]
+        x_e += [layt[edge[0]][0], layt[edge[1]][0], None]
+        y_e += [layt[edge[0]][1], layt[edge[1]][1], None]
+        z_e += [layt[edge[0]][2], layt[edge[1]][2], None]
 
     trace1 = Scatter3d(x=x_e,
                        y=y_e,
                        z=z_e,
                        mode='lines',
-                       line=Line(color='rgb(125,125,125)', width=1),
+                       line=Line(color='rgb(125,125,125)', width=weights_e),
                        hoverinfo='none'
                        )
     trace2 = Scatter3d(x=x_n,
@@ -63,13 +76,13 @@ def plotly_plot_network(file_dir):
                        mode='markers',
                        name='actors',
                        marker=Marker(symbol='dot',
-                                     size=6,
-                                     color=group,
+                                     size=weights_n,
+                                     color=weights_n,
                                      colorscale='Viridis',
                                      line=Line(
                                          color='rgb(50,50,50)', width=0.5)
                                      ),
-                       text=labels,
+                       text=names_n,
                        hoverinfo='text'
                        )
 
@@ -97,7 +110,7 @@ def plotly_plot_network(file_dir):
         annotations=Annotations([
             Annotation(
                 showarrow=False,
-                text="N-Propane: <a href='https://github.com/AdamPI314/catalytic_cycle'>[1] catalytic cycle</a>",
+                text="Propane: <a href='https://github.com/AdamPI314/catalytic_cycle'>[1] catalytic cycle</a>",
                 xref='paper',
                 yref='paper',
                 x=0,
@@ -117,8 +130,8 @@ def plotly_plot_network(file_dir):
     # plot(figure_or_data, show_link=True, link_text='Export to plot.ly', validate=True, output_type='file', include_plotlyjs=True,
     #      filename='temp-plot.html', auto_open=True, image=n_none, image_filename='plot_image', image_width=800, image_height=600, config=n_none)
 
-    py.plot(fig, show_link=False, auto_open=False,
-            filename=os.path.join(file_dir, "output", "n_les-Miserables.html"))
+    py.plot(fig, show_link=True, auto_open=False,
+            filename=os.path.join(file_dir, "output", "network.html"))
 
     return
 
@@ -129,7 +142,7 @@ if __name__ == '__main__':
     print(FILE_DIR)
     I_TIME = time.time()
 
-    plotly_plot_network(FILE_DIR)    
+    plotly_plot_network(FILE_DIR, "network.json")
 
     E_TIME = time.time()
     print("Time elapsed: {0:.5} seconds".format(E_TIME - I_TIME))
