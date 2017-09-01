6b66e75e039abc2da3af1961565b25251484cf46
AdamPI314 <shirong.bai@colorado.edu>
2017-09-02 01:09:54 +0800

    node names.

--- network.py
+++ network.py
@@ -9,6 +9,7 @@ import re
 import numpy as np
 import pandas as pd
 import networkx as nx
+import parse_spe_reaction_info as psri
 
 
 def change_spe_name(spe, dict_s=None):
@@ -34,6 +35,9 @@ def init_network(file_dir, top_n=10):
     init network
     return networkx.MultiDiGraph
     """
+    spe_idx_name_dict, _ = psri.parse_spe_info(os.path.join(
+        file_dir, "output", "species_labelling.csv"))
+
     f_n_path_name = os.path.join(file_dir, "output", "pathway_name.csv")
     f_n_path_prob = os.path.join(file_dir, "output", "pathway_prob.csv")
 
@@ -53,7 +57,7 @@ def init_network(file_dir, top_n=10):
     for _, val in d_f.iterrows():
         matched_spe = re.findall(r"S(\d+)", val['name'])
         for _, spe in enumerate(matched_spe):
-            nodes.add(change_spe_name(spe))
+            nodes.add(change_spe_name(spe, spe_idx_name_dict))
 
     for _, val in enumerate(nodes):
         d_g.add_node(val, weight=0.0, label=str(val))
@@ -65,10 +69,11 @@ def init_network(file_dir, top_n=10):
         matched_reaction = re.findall(r"R(\d+)", val['name'])
 
         for idx, spe in enumerate(matched_spe):
-            d_g.node[change_spe_name(spe)]['weight'] += 1.0 * prob
+            d_g.node[change_spe_name(spe, spe_idx_name_dict)
+                     ]['weight'] += 1.0 * prob
             if idx > 0:
-                src = change_spe_name(matched_spe[idx - 1])
-                des = change_spe_name(spe)
+                src = change_spe_name(matched_spe[idx - 1], spe_idx_name_dict)
+                des = change_spe_name(spe, spe_idx_name_dict)
                 rxn = change_rxn_name(matched_reaction[idx - 1])
                 if d_g.has_edge(src, des):
                     if rxn in d_g[src][des].keys():
@@ -107,7 +112,8 @@ if __name__ == '__main__':
     TOP_N = 1000
 
     RN_OBJ = init_network(FILE_DIR, top_n=TOP_N)
-    network_to_gephi_input_file(RN_OBJ, FILE_DIR, "network_"+str(TOP_N)+".gexf")
+    network_to_gephi_input_file(
+        RN_OBJ, FILE_DIR, "network_" + str(TOP_N) + ".gexf")
 
     END_TIME = time.time()
 
