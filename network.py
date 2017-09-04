36bf50ed4c273a60609c8a9649b608eafa28070d
AdamPI <shirong.bai@colorado.edu>
2017-09-04 00:40:40 -0600

    smooth nodes and edge weight.

--- network.py
+++ network.py
@@ -30,10 +30,24 @@ def change_rxn_name(rxn, dict_r=None):
     return dict_r[str(rxn)]
 
 
-def init_network(file_dir, top_n=10):
+def rescale_array(arr, min_t=0.0, max_t=1.0):
     """
-    init network
-    return networkx.MultiDiGraph
+    rescale array
+    """
+    min_val = min(arr)
+    max_val = max(arr)
+    ratio = (max_t - min_t) / (max_val - min_val)
+
+    for idx, val in enumerate(arr):
+        arr[idx] = (val - min_val) * ratio + min_t
+    return arr
+
+
+def init_directed_network(file_dir, top_n=10):
+    """
+    init directed network
+    without parallel edges
+    return networkx.DiGraph
     """
     spe_idx_name_dict, _ = psri.parse_spe_info(os.path.join(
         file_dir, "output", "species_labelling.csv"))
@@ -47,10 +61,9 @@ def init_network(file_dir, top_n=10):
                         delimiter=',', max_rows=top_n)
 
     d_f = pd.DataFrame(np.transpose([p_n, p_p]), columns=['name', 'prob'])
-    # print(d_f)
 
-    # directed graph
-    d_g = nx.MultiDiGraph()
+    # temporary directed graph
+    d_g_tmp = nx.DiGraph()
 
     # record all nodes
     nodes = set()
@@ -60,7 +73,7 @@ def init_network(file_dir, top_n=10):
             nodes.add(change_spe_name(spe, spe_idx_name_dict))
 
     for _, val in enumerate(nodes):
-        d_g.add_node(val, weight=0.0, label=str(val))
+        d_g_tmp.add_node(val, weight=0.0, label=str(val))
 
     for _, val in d_f.iterrows():
         prob = float(val['prob'])
@@ -69,30 +82,47 @@ def init_network(file_dir, top_n=10):
         matched_reaction = re.findall(r"R(\d+)", val['name'])
 
         for idx, spe in enumerate(matched_spe):
-            d_g.node[change_spe_name(spe, spe_idx_name_dict)
-                     ]['weight'] += 1.0 * prob
+            d_g_tmp.node[change_spe_name(
+                spe, spe_idx_name_dict)]['weight'] += 1.0 * prob
             if idx > 0:
                 src = change_spe_name(matched_spe[idx - 1], spe_idx_name_dict)
-                des = change_spe_name(spe, spe_idx_name_dict)
+                dest = change_spe_name(spe, spe_idx_name_dict)
                 rxn = change_rxn_name(matched_reaction[idx - 1])
-                if d_g.has_edge(src, des):
-                    if rxn in d_g[src][des].keys():
-                        # print("found")
-                        d_g[src][des][rxn]['weight'] += 1.0 * prob
-                    else:
-                        d_g[src][des].update(
-                            {rxn: {'name': rxn, 'weight': 1.0 * prob}})
+                if d_g_tmp.has_edge(src, dest):
+                    d_g_tmp[src][dest]['weight'] += 1.0 * prob
+                    d_g_tmp[src][dest]['reactions'].add(rxn)
                 else:
-                    d_g.add_edge(src, des, key=rxn,
-                                 name=rxn, weight=1.0 * prob)
+                    d_g_tmp.add_edge(src, dest,
+                                     reactions=set([rxn]), weight=1.0 * prob)
 
-    # print(d_g.edges())
-    # for idx, val in enumerate(d_g.nodes()):
-    #     print(idx, val, d_g.node[val]['weight'])
-    # for idx, val in enumerate(d_g.edges()):
-    #     print(idx, val, d_g[val[0]][val[1]].items())
+    # update directed graph, for example,
+    # 1. reactions is originally a set, combine to get a string of reactions
+    # 2. smooth and re-normalize node weight
+    # 3. re-normalize edge weight
+    node_weight = []
+    for _, val in enumerate(d_g_tmp.nodes()):
+        node_weight.append(d_g_tmp.node[val]['weight'])
+    edge_weight = []
+    for _, val in enumerate(d_g_tmp.edges()):
+        edge_weight.append(d_g_tmp[val[0]][val[1]]['weight'])
+    node_weight = rescale_array(node_weight, 1.0, 5.0)
+    edge_weight = rescale_array(edge_weight, 1.0, 5.0)
+    # final directed graph
+    di_graph = nx.DiGraph()
+    for idx, val in enumerate(d_g_tmp.nodes()):
+        di_graph.add_node(val, weight=node_weight[idx])
+    for idx, val in enumerate(d_g_tmp.edges()):
+        src = val[0]
+        dest = val[1]
 
-    return d_g
+        rxn_set = d_g_tmp[src][dest]['reactions']
+        rxn_set = sorted(rxn_set, key=lambda x: int(x), reverse=False)
+        name = ",".join(x for x in rxn_set)
+
+        weight = edge_weight[idx]
+        di_graph.add_edge(src, dest, name=name, weight=weight)
+
+    return di_graph
 
 
 def network_to_gephi_input_file(networkx_obj, file_dir, fname="network.gexf"):
@@ -111,7 +141,7 @@ if __name__ == '__main__':
     print(FILE_DIR)
     TOP_N = 1000
 
-    RN_OBJ = init_network(FILE_DIR, top_n=TOP_N)
+    RN_OBJ = init_directed_network(FILE_DIR, top_n=TOP_N)
     network_to_gephi_input_file(
         RN_OBJ, FILE_DIR, "network_" + str(TOP_N) + ".gexf")
 
