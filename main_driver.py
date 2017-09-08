e6a26ffc59114d217b0b6f24852596167ce2cc99
AdamPI314 <shirong.bai@colorado.edu>
2017-09-07 22:08:32 -0600

    test

--- main_driver.py
+++ main_driver.py
@@ -22,24 +22,24 @@ if __name__ == '__main__':
     INIT_SPE = 62
     END_SPE = ""
 
-    # # # run dlosde
-    # job_drivers.run_dlsode(FILE_DIR, END_TIME)
+    # run dlosde
+    job_drivers.run_dlsode(FILE_DIR, END_TIME)
 
     # write specie concentration at a time to file
     job_drivers.spe_concentration_at_time_w2f(FILE_DIR, tau=TAU)
 
-    # # run monte carlo trajectory
-    # job_drivers.run_mc_trajectory(
-    #     FILE_DIR, END_TIME, n_traj=10000000, atom_followed=ATOM_FOLLOWED, init_spe=INIT_SPE, max_tau=TAU)
+    # run monte carlo trajectory
+    job_drivers.run_mc_trajectory(
+        FILE_DIR, END_TIME, n_traj=10000000, atom_followed=ATOM_FOLLOWED, init_spe=INIT_SPE, max_tau=TAU)
 
-    # # # evaluate path integral-->pathway probability
-    # job_drivers.evaluate_pathway_probability(
-    #     FILE_DIR, top_n=TOP_N, num=1, flag="", n_traj=10000, atom_followed=ATOM_FOLLOWED, init_spe=INIT_SPE, max_tau=TAU)
+    # # evaluate path integral-->pathway probability
+    job_drivers.evaluate_pathway_probability(
+        FILE_DIR, top_n=TOP_N, num=1, flag="", n_traj=10000, atom_followed=ATOM_FOLLOWED, init_spe=INIT_SPE, max_tau=TAU)
 
-    # # # convert symbolic pathway to real pathway
-    # # # with real species names and real reaction expression
-    # job_drivers.symbolic_path_2_real_path(
-    #     FILE_DIR, top_n=TOP_N, flag="", end_spe=END_SPE)
+    # # convert symbolic pathway to real pathway
+    # # with real species names and real reaction expression
+    job_drivers.symbolic_path_2_real_path(
+        FILE_DIR, top_n=TOP_N, flag="", end_spe=END_SPE)
 
     # # species count
     # job_drivers.species_count(FILE_DIR, top_n=TOP_N, norm=True)
@@ -61,8 +61,8 @@ if __name__ == '__main__':
     # job_drivers.species_production_reaction(
     #     FILE_DIR, spe='OH', top_n=TOP_N, norm=True)
 
-    # # # send email
-    # job_drivers.send_email(FILE_DIR)
+    # send email
+    job_drivers.send_email(FILE_DIR)
 
     TIME_E = time.time()
     print("running time:\t" +
