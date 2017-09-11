6dff15e3973cff365951f2526b78e2ff76da6bdc
AdamPI <shirong.bai@colorado.edu>
2017-09-10 22:20:04 -0600

    beta

--- cantera_related.py
+++ cantera_related.py
@@ -5,6 +5,7 @@ import os
 import sys
 import json
 from collections import defaultdict
+import numpy as np
 try:
     import cantera as ct
 except ImportError:
@@ -36,39 +37,55 @@ def write_spe_composition_2_file(file_dir, spe_composition, tag=None):
         json.dump(spe_composition, f_h, indent=4, sort_keys=False)
 
 
-def evaluate_rate_constant(f_n, temp=1000, pressure=1.0, buffer=None, rxn_idx=0):
+def eval_2nd_order_rate_const(f_n, temp=None, pressure=1.0, buffer=None, rxn_idx=0):
     """
     f_n is path of "chem.xml" mechanism file
     temp is temperature
     buffer a dict store buffer gas and thier pressure
     rxn_idx is reaction index
+    evaluate 2nd order rate costant
     """
     if buffer is None:
         buffer = dict()
+    if temp is None:
+        temp = [1000]
 
     spe = ct.Species.listFromFile(f_n)
     rxn = ct.Reaction.listFromFile(f_n)
     # print(S)
-    for r in rxn_idx:
-        print(rxn[r])
+    for r_t in rxn_idx:
+        print(rxn[r_t])
     gas = ct.Solution(thermo='IdealGas', kinetics='GasKinetics',
                       species=spe, reactions=rxn)
 
-    # gas.TPY = temp, pressure * ct.one_atm, 'npropyl:1.0, O2:1.0, HE:1.0'
-    gas.TPY = temp, pressure * ct.one_atm, buffer
-
-    # print(gas.reactant_stoich_coeffs())
-    # print(gas.product_stoich_coeffs())
-
     # Units are a combination of kmol, m^3 and s, convert to cm^3/molecule/s
     # 10^6/(10^3 * 6.022 * 10^23)
-    # kmol_m_s_to_molecule_cm_s = 10**6/(10**3 * 6.022 * 10**23) 
-    kmol_m_s_to_molecule_cm_s = 1e6/(1e3 * 6.022e23) 
-    print(gas.forward_rate_constants[rxn_idx] * kmol_m_s_to_molecule_cm_s)
-    # print(gas.reverse_rate_constants)
-    # print(gas.forward_rates_of_progress[rxn_idx])
+    # kmol_m_s_to_molecule_cm_s = 10**6/(10**3 * 6.022 * 10**23)
+    kmol_m_s_to_molecule_cm_s = 1e6 / (1e3 * 6.022e23)
 
-    return
+    result = []
+    for temp_t in temp:
+        gas.TPY = temp_t, pressure * ct.one_atm, buffer
+        # print(gas.forward_rates_of_progress[rxn_idx])
+        result.append(
+            gas.forward_rate_constants[rxn_idx] * kmol_m_s_to_molecule_cm_s)
+    return result
+
+
+def beta_1000_rate_constant_w2f(file_dir, beta=None, pressure=1.0, buffer=None, rxn_idx=0):
+    """
+    beta = 1000/T
+    T = 1000/beta
+    """
+    if beta is None:
+        beta = [1.0]
+    np.savetxt(os.path.join(file_dir, "output", "beta.csv"),
+               beta, fmt=".15f", delimiter=",")
+    temp = [1000.0 / b for b in beta]
+    r_c = eval_2nd_order_rate_const(os.path.join(
+        file_dir, "input", "chem.xml"), temp=temp, pressure=pressure, buffer=buffer, rxn_idx=rxn_idx)
+    np.savetxt(os.path.join(file_dir, "output", "rate_constant.csv"),
+               r_c, fmt=".15f", delimiter=",")
 
 
 if __name__ == '__main__':
@@ -78,7 +95,10 @@ if __name__ == '__main__':
     # write_spe_composition_2_file(FILE_DIR, S_C, tag="")
     BUFFER = {"npropyl": 1.0, "O2": 1.0, "HE": 1.0}
     RXN_IDX = [548, 549, 550, 551, 552, 553]
-    TEMP = 666.66666666
-    evaluate_rate_constant(os.path.join(
+    TEMP = [666.66666666]
+    BETA = [0.5, 1.0, 1.5, 2.0, 2.5]
+    eval_2nd_order_rate_const(os.path.join(
         FILE_DIR, "input", "chem.xml"), pressure=3.0, temp=TEMP, buffer=BUFFER, rxn_idx=RXN_IDX)
+    beta_1000_rate_constant_w2f(
+        FILE_DIR, beta=BETA, pressure=3.0, buffer=BUFFER, rxn_idx=RXN_IDX)
     print(FILE_DIR)
