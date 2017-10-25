fc0168d9c826aa097d581f7bafea106bea9e1eb5
AdamPI <shirong.bai@colorado.edu>
2017-10-25 04:59:24 -0600

    minor

--- make_figures.py
+++ make_figures.py
@@ -596,7 +596,9 @@ def plot_pathway_AT(file_dir, init_spe=62, atom_followed="C", tau=1.0, pathwayEn
 
     fig, a_x = plt.subplots(1, 1, sharex=True, sharey=False)
     # arguments are passed to np.histogram
-    a_x.hist(data_pa[path_idx, :], bins='auto', normed=1, facecolor='green')
+    data_hist = data_pa[path_idx, :]
+    weights = np.ones_like(data_hist)/float(len(data_hist))
+    a_x.hist(data_hist, bins='auto', weights=weights, facecolor='green')
 
     a_x.grid()
 
