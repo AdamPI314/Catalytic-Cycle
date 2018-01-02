2892c1e178d2da44247c7744254d0fc2ff0a4b3b
AdamPI314 <shirong.bai@colorado.edu>
2018-01-02 16:40:15 -0700

    minor

--- parse_spe_reaction_info.py
+++ parse_spe_reaction_info.py
@@ -111,7 +111,7 @@ def pathname_to_real_spe_reaction(spe_ind_name_dict, new_ind_reaction_dict, path
     return str_t
 
 
-def symbolic_path_2_real_path(f_n_spe, f_n_reaction, f_n_p, f_n_p_out, top_n=50, end_s_idx=None):
+def symbolic_path_2_real_path(f_n_spe, f_n_reaction, f_n_p, f_n_p_out, top_n=50, end_s_idx=None, accurate_prob=None):
     """
     read species and reaction info,
     convert path info into real species and reaction instead of index and write to file
