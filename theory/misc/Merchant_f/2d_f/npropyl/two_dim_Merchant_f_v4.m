df1f15fbc9e8ad30172c45e60e9bc903c259eeec
AdamPI314 <shirong.bai@colorado.edu>
2018-04-09 17:52:58 -0600

    minor

--- theory/misc/Merchant_f/2d_f/npropyl/two_dim_Merchant_f_v4.m
+++ theory/misc/Merchant_f/2d_f/npropyl/two_dim_Merchant_f_v4.m
@@ -7,9 +7,10 @@ spe_name = 'npropyl';
 tau = 0.777660157519;
 end_t = '0.9';
 % end_t = '0.12859156975';
-cycle = 'primary_cycle';
+cycle = 'all';
+% cycle = 'primary_cycle';
 % cycle = 'others';
-n_path = 100;
+n_path = 1000;
 
 fn_2d_f = fullfile(file_dir, ['Merchant_f_2d_S', spe_idx, '_', atom_f, '_', end_t ,'.csv']);
 
@@ -39,8 +40,9 @@ end
 
 % path index
 offset = 2;
-path_idx = [1, 2, 3];
-% path_idx = [4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100];
+path_idx = linspace(1, n_path, n_path-1+1);
+% path_idx = [1, 2, 3];
+% path_idx = linspace(3, n_path, n_path-3+1);
 for i = 1:length(path_idx)
     if i==1
         f_value = f_mat(:, offset + path_idx(i));
