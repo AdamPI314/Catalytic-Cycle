b1e772c4eb7950d32a82f2f9ec86f68949923f66
AdamPI314 <shirong.bai@colorado.edu>
2018-04-12 18:26:31 -0600

    minor

---
+++ theory/misc/ipropylooh_source_reaction_ratio/sink_reaction_ratio_top_5.m
@@ -0,0 +1,138 @@
+%% global settings
+spe_name = 'ipropylooh';
+
+%% Current file directory
+file_dir = fullfile(fileparts(mfilename('fullpath')), '..', '..', '..', '..', '..', '..', 'SOHR_DATA');
+pic_dir = fullfile(fileparts(mfilename('fullpath')));
+
+%% import time
+fn_time = fullfile(file_dir, 'output', 'time_dlsode_M.csv');
+delimiter = '';
+formatSpec = '%f%[^\n\r]';
+%% Open the text file.
+fileID = fopen(fn_time,'r');
+dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'EmptyValue' ,NaN, 'ReturnOnError', false);
+
+%% Close the text file.
+fclose(fileID);
+time_vec = dataArray{:, 1};
+%% Clear temporary variables
+clearvars fn_time delimiter formatSpec fileID dataArray ans;
+
+%% import temperature
+fn_temp = fullfile(file_dir, 'output', 'temperature_dlsode_M.csv');
+delimiter = '';
+formatSpec = '%f%[^\n\r]';
+%% Open the text file.
+fileID = fopen(fn_temp,'r');
+%% Read columns of data according to format string.
+dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'EmptyValue' ,NaN, 'ReturnOnError', false);
+%% Close the text file.
+fclose(fileID);
+%% Allocate imported array to column variable names
+temp_vec = dataArray{:, 1};
+%% Clear temporary variables
+clearvars fn_temp delimiter formatSpec fileID dataArray ans;
+
+%% import time
+fn_R = fullfile(file_dir, 'output', 'reaction_rate_dlsode_M.csv');
+delimiter = ',';
+% For more information, see the TEXTSCAN documentation.
+formatSpec = '%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%[^\n\r]';
+
+%% Open the text file.
+fileID = fopen(fn_R,'r');
+dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'EmptyValue' ,NaN, 'ReturnOnError', false);
+%% Close the text file.
+fclose(fileID);
+%% Create output variable
+reaction_R_mat = [dataArray{1:end-1}];
+%% Clear temporary variables
+clearvars fn_R delimiter formatSpec fileID dataArray ans;
+
+%% plot
+fig = figure();
+% https://www.mathworks.com/help/matlab/graphics_transition/why-are-plot-lines-different-colors.html
+% https://www.mathworks.com/help/matlab/creating_plots/customize-graph-with-two-y-axes.html
+co = [    0    0.4470    0.7410 % 1th plot
+    0.8500    0.3250    0.0980 % 2nd plot
+    0.9290    0.6940    0.1250 % 3rd plot
+    0.4940    0.1840    0.5560 % 4th plot
+    0.4660    0.6740    0.1880 % 5th plot
+%     0.3010    0.7450    0.9330 % 6th plot
+%     0.6350    0.0780    0.1840 % 7th plot
+%     0   0   1 % placeholder
+%     0   0.5   0 % placeholder
+%     0   0.75   0.75 % placeholder
+%     0.7500         0    0.7500 % placeholder
+%     0.7500    0.7500         0 % placeholder
+%     0.2500    0.2500    0.2500 % placeholder
+    1   0   0 % bl
+    ]; 
+set(fig,'defaultAxesColorOrder',co)
+%%
+tau = 0.777660157519;
+end_t = 0.9;
+
+%% plot
+R_idx = [708, 712, 717, 858, 918, 920, 924, 928, 932, 936, 940, 948, 950, 954];
+for idx=1:length(R_idx)
+    R_idx(idx) = R_idx(idx) + 1;
+end
+
+R_name = {'ipropyloo+C_3H_8=>ipropylooh+npropyl', 'ipropyloo+C_3H_8=>ipropylooh+ipropyl', 'ipropyloxy+OH=>ipropylooh', 'C_3H_6+ipropyloo=>allyl+ipropylooh', 'ipropyloo+CH_2O=>ipropylooh+HCO', 'ipropyloo+acetaldehyde=>ipropylooh+acetyl', 'ipropyloo+HO_2=>ipropylooh+O_2', 'C_2H_4+ipropyloo=>C_2H_3+ipropylooh', 'CH_3OH+ipropyloo=>CH_2OH+ipropylooh', 'acrolein+ipropyloo=>CH_2CHCO+ipropylooh', 'CH_4+ipropyloo=>CH_3+ipropylooh', 'H_2+ipropyloo=>H+ipropylooh', 'ipropyloo+C_2H_6=>ipropylooh+C_2H_5', 'ipropyloo+propanal=>ipropylooh+propionyl'};
+
+R_mat = reaction_R_mat(:, R_idx);
+
+% sort by the reaction rates around 0.5 tau, idx == 3550 for example
+sort_axis = round(0.1 * length(time_vec));
+[B,I] = sort(R_mat(sort_axis, :),'descend');
+
+for idx=1:length(I)
+    r_idx = I(idx);
+    if idx == 1
+        R_total = R_mat(:, r_idx);
+    else
+        R_total = (R_total + R_mat(:, r_idx));
+    end
+end
+
+N = 5;
+
+% graph handler
+H = gobjects(N);
+
+for idx=1:N
+    r_idx = I(idx);
+    H(idx) = semilogy(time_vec, (R_mat(:, r_idx)) ./ (R_total), ...
+        'LineWidth', 2); hold on;
+end
+
+
+
+%% settings
+set(gca,'GridLineStyle','--');
+xlabel('Time (seconds)', 'FontSize', 20);
+ylabel('Fraction', 'FontSize', 20);
+ylim([10^-10, 10^0]);
+
+%% temp
+yyaxis right
+delta_n = 1000;
+plot(time_vec, temp_vec, 'LineWidth', 2, 'color', 'r'); hold on;
+pt = scatter(time_vec(1:delta_n:end), temp_vec(1:delta_n:end), 'MarkerEdgeColor', 'r');
+ylabel('T (K)', 'FontSize', 20);
+% set(gca, 'ytick', []);
+
+%% global settings
+grid on;
+xlim([0, tau*end_t]);
+leg_h = legend([H(1);H(2);H(3);H(4);H(5)], ...
+    R_name{1,I(1)},R_name{1,I(2)},R_name{1,I(3)},R_name{1,I(4)},R_name{1,I(5)});
+set(leg_h, 'FontSize', 12, 'Box', 'off');
+set(leg_h, 'Location', 'South')
+
+
+%% save to file
+figname = strcat(spe_name, '_source_reaction_ratio_top_5', '.png');
+print(fig, fullfile(pic_dir, figname), '-r200', '-dpng');
---
+++ theory/misc/npropyloo_sink_reaction_ratio/sink_reaction_ratio_top_5.m
@@ -0,0 +1,138 @@
+%% global settings
+spe_name = 'npropyloo';
+
+%% Current file directory
+file_dir = fullfile(fileparts(mfilename('fullpath')), '..', '..', '..', '..', '..', '..', 'SOHR_DATA');
+pic_dir = fullfile(fileparts(mfilename('fullpath')));
+
+%% import time
+fn_time = fullfile(file_dir, 'output', 'time_dlsode_M.csv');
+delimiter = '';
+formatSpec = '%f%[^\n\r]';
+%% Open the text file.
+fileID = fopen(fn_time,'r');
+dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'EmptyValue' ,NaN, 'ReturnOnError', false);
+
+%% Close the text file.
+fclose(fileID);
+time_vec = dataArray{:, 1};
+%% Clear temporary variables
+clearvars fn_time delimiter formatSpec fileID dataArray ans;
+
+%% import temperature
+fn_temp = fullfile(file_dir, 'output', 'temperature_dlsode_M.csv');
+delimiter = '';
+formatSpec = '%f%[^\n\r]';
+%% Open the text file.
+fileID = fopen(fn_temp,'r');
+%% Read columns of data according to format string.
+dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'EmptyValue' ,NaN, 'ReturnOnError', false);
+%% Close the text file.
+fclose(fileID);
+%% Allocate imported array to column variable names
+temp_vec = dataArray{:, 1};
+%% Clear temporary variables
+clearvars fn_temp delimiter formatSpec fileID dataArray ans;
+
+%% import time
+fn_R = fullfile(file_dir, 'output', 'reaction_rate_dlsode_M.csv');
+delimiter = ',';
+% For more information, see the TEXTSCAN documentation.
+formatSpec = '%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%[^\n\r]';
+
+%% Open the text file.
+fileID = fopen(fn_R,'r');
+dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'EmptyValue' ,NaN, 'ReturnOnError', false);
+%% Close the text file.
+fclose(fileID);
+%% Create output variable
+reaction_R_mat = [dataArray{1:end-1}];
+%% Clear temporary variables
+clearvars fn_R delimiter formatSpec fileID dataArray ans;
+
+%% plot
+fig = figure();
+% https://www.mathworks.com/help/matlab/graphics_transition/why-are-plot-lines-different-colors.html
+% https://www.mathworks.com/help/matlab/creating_plots/customize-graph-with-two-y-axes.html
+co = [    0    0.4470    0.7410 % 1th plot
+    0.8500    0.3250    0.0980 % 2nd plot
+    0.9290    0.6940    0.1250 % 3rd plot
+    0.4940    0.1840    0.5560 % 4th plot
+    0.4660    0.6740    0.1880 % 5th plot
+%     0.3010    0.7450    0.9330 % 6th plot
+%     0.6350    0.0780    0.1840 % 7th plot
+%     0   0   1 % placeholder
+%     0   0.5   0 % placeholder
+%     0   0.75   0.75 % placeholder
+%     0.7500         0    0.7500 % placeholder
+%     0.7500    0.7500         0 % placeholder
+%     0.2500    0.2500    0.2500 % placeholder
+    1   0   0 % bl
+    ]; 
+set(fig,'defaultAxesColorOrder',co)
+%%
+tau = 0.777660157519;
+end_t = 0.9;
+
+%% plot
+R_idx = [706, 710, 856, 914, 916, 922, 926, 930, 934, 938, 942, 946, 952, 956, 960, 964, 968, 970, 982, 984, 986, 988, 990, 1069, 1078, 1080, 1082, 1084];
+for idx=1:length(R_idx)
+    R_idx(idx) = R_idx(idx) + 1;
+end
+
+R_name = {'npropyloo+C_3H_8=>npropylooh+npropyl', 'npropyloo+C_3H_8=>npropylooh+ipropyl', 'C_3H_6+npropyloo=>allyl+npropylooh', 'npropyloo+CH_2O=>npropylooh+HCO', 'npropyloo+acetaldehyde=>npropylooh+acetyl', 'npropyloo+HO_2=>npropylooh+O_2', 'C_2H_4+npropyloo=>C_2H_3+npropylooh', 'CH_3OH+npropyloo=>CH_2OH+npropylooh', 'acrolein+npropyloo=>CH_2CHCO+npropylooh', 'CH_4+npropyloo=>CH_3+npropylooh', 'npropyloo+CH_3OO=>npropyloxy+CH_3O+O_2', 'H_2+npropyloo=>H+npropylooh', 'npropyloo+C_2H_6=>npropylooh+C_2H_5', 'npropyloo+propanal=>npropylooh+propionyl', 'npropyloo+acetylperoxy=>npropyloxy+acetyloxy+O_2', 'npropyloo+CH_3CH_2OO=>npropyloxy+ethoxy+O_2', 'npropyloo+npropyloo=>O_2+npropyloxy+npropyloxy', 'ipropyloo+npropyloo=>ipropyloxy+npropyloxy+O_2', 'npropyloo+CH_3=>npropyloxy+CH_3O', 'npropyloo+C_2H_5=>npropyloxy+ethoxy', 'npropyloo+ipropyl=>npropyloxy+ipropyloxy', 'npropyloo+npropyl=>npropyloxy+npropyloxy', 'npropyloo+allyl=>npropyloxy+allyloxy', 'npropyloo=>O_2+npropyl', 'npropyloo=>QOOH_2', 'npropyloo=>QOOH_1', 'npropyloo=>HO_2+C_3H_6', 'npropyloo=>OH+propoxide'};
+
+R_mat = reaction_R_mat(:, R_idx);
+
+% sort by the reaction rates around 0.5 tau, idx == 3550 for example
+sort_axis = round(0.1 * length(time_vec));
+[B,I] = sort(R_mat(sort_axis, :),'descend');
+
+for idx=1:length(I)
+    r_idx = I(idx);
+    if idx == 1
+        R_total = R_mat(:, r_idx);
+    else
+        R_total = (R_total + R_mat(:, r_idx));
+    end
+end
+
+N = 5;
+
+% graph handler
+H = gobjects(N);
+
+for idx=1:N
+    r_idx = I(idx);
+    H(idx) = semilogy(time_vec, (R_mat(:, r_idx)) ./ (R_total), ...
+        'LineWidth', 2); hold on;
+end
+
+
+
+%% settings
+set(gca,'GridLineStyle','--');
+xlabel('Time (seconds)', 'FontSize', 20);
+ylabel('Fraction', 'FontSize', 20);
+ylim([10^-3.5, 10^0]);
+
+%% temp
+yyaxis right
+delta_n = 1000;
+plot(time_vec, temp_vec, 'LineWidth', 2, 'color', 'r'); hold on;
+pt = scatter(time_vec(1:delta_n:end), temp_vec(1:delta_n:end), 'MarkerEdgeColor', 'r');
+ylabel('T (K)', 'FontSize', 20);
+% set(gca, 'ytick', []);
+
+%% global settings
+grid on;
+xlim([0, tau*end_t]);
+leg_h = legend([H(1);H(2);H(3);H(4);H(5)], ...
+    R_name{1,I(1)},R_name{1,I(2)},R_name{1,I(3)},R_name{1,I(4)},R_name{1,I(5)});
+set(leg_h, 'FontSize', 12, 'Box', 'off');
+set(leg_h, 'Location', 'South')
+
+
+%% save to file
+figname = strcat(spe_name, '_source_reaction_ratio_top_5', '.png');
+print(fig, fullfile(pic_dir, figname), '-r200', '-dpng');
