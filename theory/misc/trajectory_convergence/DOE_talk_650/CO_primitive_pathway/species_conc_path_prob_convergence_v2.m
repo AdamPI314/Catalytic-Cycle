87bb076f0765987c47b2469d87d1e90e7410b61f
AdamPI314 <shirong.bai@colorado.edu>
2018-06-18 19:40:21 -0600

    minor

--- theory/misc/trajectory_convergence/DOE_talk_650/CO_primitive_pathway/species_conc_path_prob_convergence_v2.m
+++ theory/misc/trajectory_convergence/DOE_talk_650/CO_primitive_pathway/species_conc_path_prob_convergence_v2.m
@@ -16,6 +16,8 @@ end_t = '0.25718313951098054';
 % stage 1B end time
 % end_t = '0.720108899222239';
 n_path = 100;
+% pre_factor = 7.44277901848944108e-06 * 1.0 * 3.0;
+pre_factor = 1.0;
 
 %% import time
 f_n_scc_time = fullfile(file_dir, [prefix, 'scc_time.csv']);
@@ -28,7 +30,7 @@ scc_time_vec = dataArray{:, 1};
 clearvars f_n_scc_time delimiter formatSpec fileID dataArray ans;
 
 %% import spe conc
-f_n_conc = fullfile(file_dir, [prefix, 'spe_conc_converted_to_pp.csv']);
+f_n_conc = fullfile(file_dir, [prefix, 'spe_conc.csv']);
 delimiter = ',';
 formatStr = '';
 for i=1:110
@@ -39,7 +41,7 @@ formatSpec = char(formatStr);
 fileID = fopen(f_n_conc,'r');
 dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'EmptyValue', NaN,  'ReturnOnError', false);
 fclose(fileID);
-spe_conc_in_pp = [dataArray{1:end-1}];
+spe_conc = [dataArray{1:end-1}];
 clearvars f_n_conc delimiter formatSpec fileID dataArray ans;
 
 %% import pathway prob
@@ -79,10 +81,10 @@ data_x = scc_time_vec * tau;
 color_idx = 1;
 for idx =1:N+1
     if idx == N+1
-        data_y = spe_conc_in_pp(:, spe_idx + 1);
+        data_y = spe_conc(:, spe_idx + 1);
     else
         A = scc_pp(:, 1:idx_vec(idx));
-        data_y = sum(A, 2);
+        data_y = pre_factor * sum(A, 2);
     end
 
     plot(data_x, data_y, ...
